56d09593be78a0dc60696fe679561349045c9e46
lucas <mr.pine97@gmail.com>
2022-01-17 15:22:28 -0300

    feat: req2 COMPLETO: cria a rota talker/:id atrelada ao middleware getPanelistsById e reorganiza as rotas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const getAllPanelists = require('./middlewares/getAllPanelists');
+const getPanelistById = require('./middlewares/getPanelistById');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,12 +9,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.get('/talker', getAllPanelists);
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', getAllPanelists);
+
+app.get('/talker/:id', getPanelistById);
 
 app.listen(PORT, () => {
   console.log('Online');
