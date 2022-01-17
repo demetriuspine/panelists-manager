bf313cc99d7c63f482da454b2fd9e3268fd6764a
lucas <mr.pine97@gmail.com>
2022-01-17 20:18:08 -0300

    fix: req3: adiciona referencia

--- utils/tokenGeneration.js
+++ utils/tokenGeneration.js
@@ -1,6 +1,6 @@
 const { randomBytes } = require('crypto');
 
-const numberOfBytes = 8; // numero atingido por tentativa e erro
+const numberOfBytes = 8; // https://trybecourse.slack.com/archives/C023YHXAEGM/p1642459001138500
 
 function tokenGeneration() { // https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js e https://nodejs.org/api/crypto.html#cryptorandombytessize-callback
   return randomBytes(numberOfBytes).toString('hex');
