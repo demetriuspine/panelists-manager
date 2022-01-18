df894ff76bc6f789fb117ecf3dd38c25a01c9663
lucas <mr.pine97@gmail.com>
2022-01-18 16:53:58 -0300

    refact: req5: atribui o rate convertido a uma variavel

--- middlewares/panelistValidations/rateValidation.js
+++ middlewares/panelistValidations/rateValidation.js
@@ -1,10 +1,12 @@
 function rateValidation(req, res, next) {
   const { rate } = req.body.talk;
 
-  if (Number(rate) < 1
-  || Number(rate) > 5
-  || Number.isInteger(Number(rate)) === false // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
-  || Number.isNaN(Number(rate))) { // https://www.w3schools.com/jsref/jsref_isnan_number.asp
+  const convertedRate = Number(rate);
+
+  if (convertedRate < 1
+  || convertedRate > 5
+  || Number.isInteger(convertedRate) === false // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+  || Number.isNaN(convertedRate)) { // https://www.w3schools.com/jsref/jsref_isnan_number.asp
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
     }
 
