f8fcbfb985bafbc56c56e99aa63a330d66512cce
lucas <mr.pine97@gmail.com>
2022-01-18 16:55:06 -0300

    feat: req5: cria o middleware editPanelistById

---
+++ middlewares/editPanelistById.js
@@ -0,0 +1,24 @@
+const { promises } = require('fs');
+
+const indexRelativeFilepath = './talker.json';
+
+async function editPanelistById(req, res) {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const updatedPanelist = { id: Number(id), name, age, talk };
+  try {
+    const data = await promises.readFile(indexRelativeFilepath, 'utf-8');
+    const parsedData = await JSON.parse(data);
+    const panelistIndex = parsedData.findIndex((talker) => `${talker.id}` === id);
+    if (panelistIndex === -1) { // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    parsedData[panelistIndex] = updatedPanelist;
+    await promises.writeFile(indexRelativeFilepath, JSON.stringify(parsedData));
+    res.status(200).json(updatedPanelist);
+  } catch (err) {
+    console.error(`Erro ao ler ou escrever o arquivo: ${err.message}`);
+  }
+}
+
+module.exports = editPanelistById;
